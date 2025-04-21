c40115548b67e9a0f94ac69d10d16027d34ceea2
Sanchia Menezes <sanchia.menezes19@gmail.com>
2025-04-21 16:09:58 -0400

    Starter code for unit tests

---
+++ src/components/UserListComponent/UserListComponent.test.tsx
@@ -0,0 +1,24 @@
+// Starter code for UT
+// describe("UserListComponent", () => {
+//   afterEach(() => {});
+
+//   it("renders loading state", () => {
+//     render(<UserListComponent />);
+//     expect(screen.getByText(/loading/i)).toBeInTheDocument();
+//   });
+
+//   it("renders error state", () => {
+//     render(<UserListComponent />);
+//     expect(screen.getByText(/error loading users/i)).toBeInTheDocument();
+//   });
+
+//   it("renders no data state", () => {
+//     render(<UserListComponent />);
+//     expect(screen.getByText(/no user data/i)).toBeInTheDocument();
+//   });
+
+//   it("renders user table with data", () => {
+//     render(<UserListComponent />);
+//     expect(screen.getByText(/ABC/i)).toBeInTheDocument();
+//   });
+// });
